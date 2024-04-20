2417b94e8f945a6031aa6f73b999dfaeecd9aab7
Rebekka Nordheim <RebNor46673@stud.noroff.no>
2024-04-20 17:27:13 +0200

    saving changes from feedback

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,35 +1,3 @@
-/* import React from 'react';
-import '../../App.css';
-import { Link } from 'react-router-dom';
-
-
-function Header() {
-  return (
-    <div>
-      <nav className='nav'>
-        <ul>
-          <li>
-            <Link to='/'>Home</Link>
-          </li>
-          <li>
-            <Link to='/products'>Products</Link>
-          </li>
-          <li>
-            <Link to='/contact'>Contact</Link>
-          </li>
-          <li>
-            <Link to='/cart'>Cart</Link>
-          </li>
-          
-        </ul>
-      </nav>
-    </div>
-  );
-}
-
-export default Header;
-
- */
 import React from 'react';
 import '../../App.css';
 import { Link } from 'react-router-dom';
